de22ce1ccd477f89cd7caa5d3149d6e23354e26f
bella <bella.royle@gmail.com>
2020-11-13 14:23:40 +0000

    setting up hosting

--- models/api.js
+++ models/api.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises
 
 exports.fetchEndpoints = () => {
     return fs.readFile('endpoints.json', 'utf8')
-        .then(endpoint => {
-            return JSON.parse(endpoint)
-        })
+    //         .then(endpoint => {
+    //             return JSON.parse(endpoint)
+    //         })
 }
